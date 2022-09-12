c74b61ebc8803d24ad789ac9beb08a50e47ebc09
Sayan Batabyal <83637322+Sayan-Batabyal@users.noreply.github.com>
2022-09-13 00:00:42 +0530

    Create Pangram.cpp

---
+++ Pangram.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+bool approach1(string s){
+    if(s.length()<26)
+        return false;
+    for (char i = 'a'; i <='z' ; ++i) {
+        bool found=false;
+        for (int j = 0; j < s.length(); ++j) {
+            if(s[j]==i)
+                found=true;
+        }
+        if(!found)
+            return false;
+    }
+    return true;
+}
+bool approach2(string s){
+    bool ar[26]={false};
+    for (int i = 0; i <s.length() ; ++i) {
+        ar[s[i]-'a']=true;
+    }
+    for (int i = 0; i <26 ; ++i) {
+        if(!ar[i])
+            return false;
+    }
+    return true;
+}
+int main(){
+    string s="abcdefghijklmnopqrstuvwxyz";
+    cout<<"Pangram: "<<approach2(s);
+    return 0;
+}
